4b59325b3677d9ae3c4a74c4d3de624b1dacaabc
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-05-15 09:48:33 +0300

    changes

--- filter_variants.py
+++ filter_variants.py
@@ -2,12 +2,16 @@ import numpy as np
 import os
 import gzip
 from operator import itemgetter
+import os
+import shutil
+
+plink = shutil.which('plink')
 
 import pickle
 currPath = os.getcwd() + '/'
 dataPath =  'Data/'
-filePath =  dataPath + 'annotated_variants.gz'
-
+annotatedVariants =  dataPath + 'annotated_variants.gz'
+bashPath = 'tmp_scripts/'
 from collections import defaultdict as dd
 
 import shlex
@@ -33,7 +37,7 @@ def create_info_file():
     Creates a filtered_variants_score.txt file which only contains the variants with minimum IS across batches >= filt
     '''
     
-    with gzip.open(filePath,'rt') as i,open(dataPath + 'lof_variants.txt','wt') as o:
+    with gzip.open(annotatedVariants,'rt') as i,open(dataPath + 'lof_variants.txt','wt') as o:
         infoPos,lofPos,avgPos,genePos = read_header(i.readline().strip().split('\t'))
 
         for line in i:
@@ -46,13 +50,23 @@ def create_info_file():
             if (lof == "true"):
                 o.write(variant.replace(':','_') + '\t' + gene + '\n')
 
+    shPath = bashPath +  'snplist.sh'
+    cmd = "cat Data/lof_variants.txt | cut -f1 >> Data/lof.snplist"
+    with open(shPath,'wt') as o:
+        o.write(' #!/bin/bash\n')
+        o.write(cmd)
+
+    call(['chmod','+x',shPath])
+    call(shPath,shell = True)
+
+
 def read_header(header = None):
     '''
     Reads the first line of the variants.gz file and returns the position of the Info score
     '''
     
     if header == None:
-        with gzip.open(filePath,'rt') as i:
+        with gzip.open(annotatedVariants,'rt') as i:
     
             header = i.readline()
             header = header.strip().split('\t')
@@ -64,3 +78,8 @@ def read_header(header = None):
     genePos =  [i for i,elem in enumerate(header) if  elem == "gene"][0]
     return infoPos,lofPos,avgPos,genePos
 
+
+def plink_filter(filePath,geno = 0.9):
+
+    
+    cmd = 'plink -bfile ' + filePath + ' --
