616ee60c9989a33df0b57268f39411ab1e298cf6
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-05-15 11:01:57 +0300

    changes

--- filter_variants.py
+++ filter_variants.py
@@ -35,7 +35,7 @@ def generate_matrix(iFile,oFile):
     cmd = 'plink -bfile '+ iFile +' --recode A --out ' + oFile
     call(shlex.split(cmd))
     #remove unncessary columns
-    cmd = "cat " +oFile + " |cut -d ' ' -f-2,7- > lof_matrix.tsv "
+    cmd = "cat " +oFile + ".raw |cut -d ' ' -f-2,7- > " + oFile
     shPath = bashPath +  'filter_lof_matrix.sh'
 
     with open(shPath,'wt') as o:
