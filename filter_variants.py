9ef9520801c45be96768d9e8dc0b6038c0bc3d6b
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-05-16 15:28:56 +0300

    changes

--- filter_variants.py
+++ filter_variants.py
@@ -89,6 +89,20 @@ def get_variant_to_gene_dict(iPath):
             g2v[gene].append(variant)
     return g2v
 
+def return_header_variants(iPath):
+    '''
+    The plink command adds the alt to the name of the variant. Here i loop through the variants and just return the original variant name
+    '''
+    with open(iPath + matrixName,'rt') as i:
+        header = i.readline()
+        header = header.strip().split(' ')[1:]
+        headerVariants = ['_'.join(elem.split('_')[:-1]) for elem in header]
+        
+    return np.array(headerVariants,dtype = str)
+                
+                      
+
+
 
 #######################
 #--GENERATING MATRIX--#
