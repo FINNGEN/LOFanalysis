7c6c585d29179d48fa38047a67ab71bd2ae2ebcf
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-05-22 12:52:16 +0300

    change

--- Scripts/filter_variants.py
+++ Scripts/filter_variants.py
@@ -57,16 +57,12 @@ def return_gene_columns(gene,iPath,g2v,headerVariants,lofString = 'hc_lof'):
     print(gene,geneColumns)
 
     #import sample data keeping columns of gene
-    vData = np.loadtxt(matrixPath,dtype = str,usecols = geneColumns)
-    #convert NA to 0
-    vData[vData =='NA'] = 0
-    #convert to int    
-    vData = vData.astype(int)        
+    vData = np.loadtxt(matrixPath,dtype = float,usecols = geneColumns)
     if len(geneColumns) > 1:
         #sum across variants and check if >1
-        vData = np.sum(vData,axis = 1)
+        vData = np.max(vData,axis = 1)
     
-    return (vData>0).astype(int)
+    return vData
 
 
 def get_variant_to_gene_dict(iPath,lofString = 'hc_lof'):
