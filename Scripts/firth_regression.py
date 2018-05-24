2ba711a6520c6f2c5b436b6cedc08fda377f05fb
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-05-24 12:05:32 +0300

    changes

--- Scripts/firth_regression.py
+++ Scripts/firth_regression.py
@@ -4,7 +4,6 @@ import numpy as np
 def fit_firth(y, X, start_vec = None, step_limit=1000, convergence_limit=0.0001):
 
     logit_model = smf.Logit(y, X)
-    return logit_model
     if start_vec is None:
         start_vec = np.zeros(X.shape[1])
     
@@ -20,7 +19,7 @@ def fit_firth(y, X, start_vec = None, step_limit=1000, convergence_limit=0.0001)
         H = np.dot(np.transpose(X), np.transpose(rootW))
         H = np.matmul(var_covar_mat, H)
         H = np.matmul(np.dot(rootW, X), H)
-
+        return pi,H
         # penalised score
         U = np.matmul(np.transpose(X), pi.values - pi + np.multiply(np.diagonal(H), 0.5 - pi))
         new_beta = beta_iterations[i] + np.matmul(var_covar_mat, U)
