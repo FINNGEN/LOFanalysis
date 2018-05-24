036669aff2bdbd6db7402abf4a9acc2b80f3e718
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-05-25 00:02:35 +0300

    changes

--- Scripts/filter_variants.py
+++ Scripts/filter_variants.py
@@ -118,7 +118,8 @@ def return_gene_columns(gene,iPath,g2v,headerVariants,lofString = 'hc_lof'):
     Loops through the header of the matrix file and returns the columns where variants belong to the gene
     """
     geneVariants = g2v[gene]
-    matrixPath = iPath +  lofString + '_info_score_matrix.txt'
+
+    matrixPath = iPath + '/plink_files/'+ lofString + matrixName
 
     geneColumns = [i+1 for i,elem in enumerate(headerVariants) if elem in geneVariants]
     print(gene,geneColumns)
@@ -157,137 +158,6 @@ def get_variant_to_gene_dict(iPath,lofString = 'hc_lof'):
 
 
 
-###############################################
-#---CONVERTS LOF MATRIX TO INFO_SCORE MATIX---#
-###############################################
-def write_info_score_matrix(annotatedPath,oPath,lofString,batchPath = dataPath + 'sample_info.txt'):
-
-    '''
-    Goes through each line of the matrix(sample data) and updates the 1s to be the Info score for that sample's batch.
-    process_line() is called for each line of the matrix
-    '''
-    
-    oFile = oPath + lofString + '_info_score_matrix.txt'
-
-    if os.path.isfile(oFile):
-        print('info score matrix already generated')
-
-    else:
-        print('generating info score matrix..')
-        matrixPath = oPath + '/plink_files/'+ lofString + matrixName
-
-        #stuff required  
-        vDict = variant_is_dict(annotatedPath,oPath,lofString)
-        s2b = sample_to_batch_ditct(batchPath)
-        headerVariants = return_header_variants(matrixPath)
-    
-        print('looping samples...')
-        with open(matrixPath,'rt') as i,open(oFile,'wt') as o:
-            next(i) #skip header
-            for line in i:
-                sample,data = process_line(line,s2b,headerVariants,vDict)
-                o.write(sample + ' ' + ' '.join([str(elem) for elem in data]) + '\n')
-    
-
-def process_line(line,s2b,headerVariants,vDict):
-    '''
-    Given a line of the lof_matrix, it returns 0 if not lof and INFO_SCORE of the batch for that variant otherwise.
-    It uses
-    '''
-    line = line.strip().split(' ')
-    # get sample info
-    sample = line[0]
-    batch = s2b[sample]
-    #read data from sample
-    data = np.array(line[1:],dtype = str)
-    data = np.isin(data,['1','2']).astype(float) # boolean if lof or not
-    #now we have a 1 if there is lof and 0 elsewhere
-    dataMask = np.where(data==1)[0] #index of variant with lof
-    # now i create a mini array only for the positions with lof and i multiply the original array, masking it, with the info-score array
-    infoArray = np.empty(len(dataMask),dtype = float)
-    for i,elem in enumerate(infoArray):
-        lofVariant = headerVariants[i]
-        infoArray[i] = vDict[lofVariant][batch]
-    data[dataMask] *= infoArray
-
-    return sample,data
-    
-
-
-
-def variant_is_dict(annVariants = annotatedVariants,iPath ='/home/pete/results/hc_lof/',lofString = "hc_lof" ):
-    
-    '''
-    Read the annotated_variants and returns a dict[variant][batch] = INFO_SCORE for teh variants that are in the snplist.
-    I can use this dictionary to retreieve the info score for the samples
-    '''
-
-    picklePath = iPath + lofString + '_vDict.p'
-    print('loading/generating dict[variant][batch] = INFO_SCORE dict -->' + picklePath)
-    try:
-        vDict = pickle.load(open(picklePath,'rb'))
-    except:
-        snplist = iPath + '/plink_files/' +lofString + '.snplist'
-
-        print('data missing, generating..')
-        variants = np.loadtxt(snplist,dtype = str)   
-        vDict = defaultdict(dd_str)
-        with gzip.open(annVariants,'rt') as i:
-            #read header
-            header = i.readline().strip().split('\t')
-            infoPos,lofPos,avgPos,genePos = read_header(header)
-            #return position of batches 
-            batches = header[infoPos[0]:infoPos[-1]+1]
-            #return batches
-            batches = [batch.split('INFO_')[1].split('_R1')[0] for batch in batches]
-            pickle.dump(batches,open(dataPath + 'ourbatches.p','wb'))
-        
-            startPos = infoPos[0]
-            rangebatches = np.arange(len(batches))
-            assert len(batches) == len(infoPos)
-
-            #loop variants
-            for line in i:
-                line = line.strip().split('\t')
-                variant = line[0].replace(':','_')
-                if variant in variants:
-                    for b in rangebatches:
-                        batch = batches[b]
-                        vDict[variant][batch] = line[startPos + b]
-
-        pickle.dump(vDict,open(picklePath,'wb'))
-
-    return vDict
-
-
-
-
-def sample_to_batch_ditct(filePath = dataPath + 'sample_info.txt'):
-    '''
-    Given timo's file maps a sample to a batch. requires a conversion on the fly due to slightly different names between his batch names and ours. Need to pass our batches and use difflib
-    '''
-
-    picklePath = dataPath +  'sample_to_batch.p'
-
-    try:
-        print('returning sample to batch dict')
-        s2b = pickle.load(open(picklePath,'rb'))
-        
-    except:
-        print("data doesn't exist..generating..")
-        import difflib                         
-        ourbatches = pickle.load(open(dataPath + 'ourbatches.p','rb'))
-
-                             
-        s2b = dd(str)
-        sampleData = np.loadtxt(filePath,dtype = str,delimiter=':',usecols=[0,-1])
-        for entry in sampleData:
-            timoBatch,sample = entry
-            ourBatch = difflib.get_close_matches(timoBatch,ourbatches)[0]
-            s2b[sample] = ourBatch
-
-        pickle.dump(s2b,open(picklePath,'wb'))
-    return s2b
 
 
 #######################
@@ -428,8 +298,6 @@ if __name__ == '__main__':
         plink_filter(args.plinkPath,oPath,args.geno,args.lof)
         # build the plink lof matrix
         generate_matrix(oPath,args.lof)
-        # convert 1s and 2s to Info score
-        write_info_score_matrix(args.annotatedFile,oPath,args.lof)
-        #from the info score matrix merge variants into genes
+        #from the plink matrix merge variants into genes
         do_chunks(oPath,args.lof)
         write_gene_matrix(oPath,args.lof)
--- Scripts/stuff.py
+++ Scripts/stuff.py
@@ -1,34 +1,54 @@
-####################################
-#--GET INFO SCORE OF LOF VARIANTS--#
-####################################
 
-def write_info_score_matrix(annotatedPath,snplist,batchPath,matrixPath,oPath):
-    s2b = sample_to_batch_ditct(batchPath)
-    vDict = variant_is_dict(snplist)
-    headerVariants = return_header_variants(matrixPath)
-    with open(matrixPath,'rt') as i,open(oPath,'wt') as o:
-        next(i) #skip header
-        for line in i:
-            sample,data = process_line(line,s2b,headerVariants,vDict)
-            o.write(sample + '\t' + '\t'.join([str(elem) for elem in data]) + '\n')
+
+
+
+###############################################
+#---CONVERTS LOF MATRIX TO INFO_SCORE MATIX---#
+###############################################
+def write_info_score_matrix(annotatedPath,oPath,lofString,batchPath = dataPath + 'sample_info.txt'):
+
+    '''
+    Goes through each line of the matrix(sample data) and updates the 1s to be the Info score for that sample's batch.
+    process_line() is called for each line of the matrix
+    '''
     
-              
-            
+    oFile = oPath + lofString + '_info_score_matrix.txt'
 
+    if os.path.isfile(oFile):
+        print('info score matrix already generated')
+
+    else:
+        print('generating info score matrix..')
+        matrixPath = oPath + '/plink_files/'+ lofString + matrixName
+
+        #stuff required  
+        vDict = variant_is_dict(annotatedPath,oPath,lofString)
+        s2b = sample_to_batch_ditct(batchPath)
+        headerVariants = return_header_variants(matrixPath)
+    
+        print('looping samples...')
+        with open(matrixPath,'rt') as i,open(oFile,'wt') as o:
+            next(i) #skip header
+            for line in i:
+                sample,data = process_line(line,s2b,headerVariants,vDict)
+                o.write(sample + ' ' + ' '.join([str(elem) for elem in data]) + '\n')
     
 
 def process_line(line,s2b,headerVariants,vDict):
     '''
-    Given a line of the lof_matrix, it returns 0 if not lof and INFO_SCORE of the batch for that variant otherwise
+    Given a line of the lof_matrix, it returns 0 if not lof and INFO_SCORE of the batch for that variant otherwise.
+    It uses
     '''
     line = line.strip().split(' ')
+    # get sample info
     sample = line[0]
     batch = s2b[sample]
+    #read data from sample
     data = np.array(line[1:],dtype = str)
-    data = np.isin(data,['1','2']).astype(float)
+    data = np.isin(data,['1','2']).astype(float) # boolean if lof or not
     #now we have a 1 if there is lof and 0 elsewhere
     dataMask = np.where(data==1)[0] #index of variant with lof
-    # now i create a mini array that will multiply the 1s
+    # now i create a mini array only for the positions with lof and i multiply the original array, masking it, with the info-score array
     infoArray = np.empty(len(dataMask),dtype = float)
     for i,elem in enumerate(infoArray):
         lofVariant = headerVariants[i]
@@ -40,26 +60,33 @@ def process_line(line,s2b,headerVariants,vDict):
 
 
 
-
-#---VARIANT/SAMPLE/INFO_SCORE DICT--#
-def variant_is_dict(snplist ='/home/pete/lof_data/filtered_lof.snplist' ):
+def variant_is_dict(annVariants = annotatedVariants,iPath ='/home/pete/results/hc_lof/',lofString = "hc_lof" ):
     
     '''
-    Read the annotated_variants and returns a dict[variant][batch] = INFO_SCORE for teh variants that are in the snplist
+    Read the annotated_variants and returns a dict[variant][batch] = INFO_SCORE for teh variants that are in the snplist.
+    I can use this dictionary to retreieve the info score for the samples
     '''
 
+    picklePath = iPath + lofString + '_vDict.p'
+    print('loading/generating dict[variant][batch] = INFO_SCORE dict -->' + picklePath)
     try:
-        print('pickling..')
-        vDict = pickle.load(open(dataPath + 'vDict.p','rb'))
+        vDict = pickle.load(open(picklePath,'rb'))
     except:
+        snplist = iPath + '/plink_files/' +lofString + '.snplist'
+
         print('data missing, generating..')
         variants = np.loadtxt(snplist,dtype = str)   
         vDict = defaultdict(dd_str)
-        with gzip.open(annotatedVariants,'rt') as i:
+        with gzip.open(annVariants,'rt') as i:
+            #read header
             header = i.readline().strip().split('\t')
             infoPos,lofPos,avgPos,genePos = read_header(header)
+            #return position of batches 
             batches = header[infoPos[0]:infoPos[-1]+1]
+            #return batches
             batches = [batch.split('INFO_')[1].split('_R1')[0] for batch in batches]
+            pickle.dump(batches,open(dataPath + 'ourbatches.p','wb'))
+        
             startPos = infoPos[0]
             rangebatches = np.arange(len(batches))
             assert len(batches) == len(infoPos)
@@ -73,10 +100,36 @@ def variant_is_dict(snplist ='/home/pete/lof_data/filtered_lof.snplist' ):
                         batch = batches[b]
                         vDict[variant][batch] = line[startPos + b]
 
-        pickle.dump(vDict,open(dataPath + 'vDict.p','wb'))
+        pickle.dump(vDict,open(picklePath,'wb'))
 
     return vDict
 
 
 
 
+def sample_to_batch_ditct(filePath = dataPath + 'sample_info.txt'):
+    '''
+    Given timo's file maps a sample to a batch. requires a conversion on the fly due to slightly different names between his batch names and ours. Need to pass our batches and use difflib
+    '''
+
+    picklePath = dataPath +  'sample_to_batch.p'
+
+    try:
+        print('returning sample to batch dict')
+        s2b = pickle.load(open(picklePath,'rb'))
+        
+    except:
+        print("data doesn't exist..generating..")
+        import difflib                         
+        ourbatches = pickle.load(open(dataPath + 'ourbatches.p','rb'))
+
+                             
+        s2b = dd(str)
+        sampleData = np.loadtxt(filePath,dtype = str,delimiter=':',usecols=[0,-1])
+        for entry in sampleData:
+            timoBatch,sample = entry
+            ourBatch = difflib.get_close_matches(timoBatch,ourbatches)[0]
+            s2b[sample] = ourBatch
+
+        pickle.dump(s2b,open(picklePath,'wb'))
+    return s2b
