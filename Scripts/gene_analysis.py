ab681b33090fade215e85d59776f399b1c5759e5
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-05-24 14:26:23 +0300

    changes

--- Scripts/gene_analysis.py
+++ Scripts/gene_analysis.py
@@ -335,7 +335,7 @@ if __name__ == '__main__':
                         help="path to eigenvec file",required = False,default = eigenvecPath)
     parser.add_argument("--lof", type= str,
                         help="type of lof filter",required = True )
-    parser_fix_samples.add_argument("--oPath", type= str,help="Path to folder where to output",default = ".")
+    parser.add_argument("--oPath", type= str,help="Path to folder where to output",default = ".")
 
     subparsers = parser.add_subparsers(help='help for subcommand',dest ="command")
 
