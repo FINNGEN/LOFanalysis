4b1e7a381fd543d9aa24e7f91cde6f9d3038d4bb
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-05-25 15:00:51 +0300

    changes

--- Scripts/gene_analysis.py
+++ Scripts/gene_analysis.py
@@ -58,7 +58,7 @@ def logistic_pheno(iPath,pheno,lofString = 'hc_lof',f = phenoFile,test = True,in
         np.savetxt(phenoSave,phenoData,fmt = '%i')
         print('phenoData imported')        
 
-
+    return 1
     pcPath = iPath + lofString + '_pcs.txt'
     pcData = np.loadtxt(pcPath,dtype = float,usecols = range(1,11))
     print('pcData loaded.')
