cb686ed19deddacfd62d3db94c9097a11c195d37
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-05-28 11:36:09 +0300

    changes

--- Scripts/analysis_results.py
+++ Scripts/analysis_results.py
@@ -25,7 +25,7 @@ def write_final_file(iPath,lofString = 'hc_lof'):
                 for line in i:
                     o.write(gene + '\t' + line)
     
-    cmd = 'cat ' +filePath + ' |  cut -f-2,8 | sort -k3 > ' + iPath + lofString + '_gene_summary_ordered.txt'
+    cmd = 'cat ' +filePath + ' | sort -k8 > ' + iPath + lofString + '_gene_summary_ordered.txt'
 
     shPath = bashPath +  'filter_lof_matrix.sh'
 
