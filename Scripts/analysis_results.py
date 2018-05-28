497a966e202934e34195b6fbf8c529c8108e5a6c
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-05-28 11:11:39 +0300

    changes

---
+++ Scripts/analysis_results.py
@@ -0,0 +1,19 @@
+import numpy as np
+import os
+from operator import itemgetter
+import pickle
+import shlex
+import sys
+from subprocess import Popen, PIPE,call
+from file_utils import make_sure_path_exists,return_header_variants,split_array_chunk,read_header,get_filepaths
+
+
+rootPath = '/'.join(os.path.realpath(__file__).split('/')[:-2]) + '/'
+
+
+def write_final_file(iPath,lofString = 'hc_lof'):
+
+    oPath = iPath + '/gene_fits/'
+    fileList = [p for p in get_filepaths(oPath)]
+    return fileList
+    
