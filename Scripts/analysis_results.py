50c69ee7ed3eed0d7b31833b7332b2a4f362bb67
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-05-28 14:33:51 +0300

    changes

--- Scripts/analysis_results.py
+++ Scripts/analysis_results.py
@@ -30,7 +30,7 @@ def write_final_file(iPath,lofString = 'hc_lof'):
                     pval = line[-1]
                     line[-1] = format_e(float(pval))
                     line = '\t'.join(line)
-                    o.write(gene + '\t' + line)
+                    o.write(gene + '\t' + line + '\n')
 
     oFile =  iPath + lofString + '_gene_summary_ordered.txt'
     cmd = 'echo gene lof_cases lof_controls no_lof_cases no_lof_controls fischer_oddsratio fischer_pval > ' + oFile +  ' && cat ' +filePath + ' | sort -k8 >> ' + oFile
