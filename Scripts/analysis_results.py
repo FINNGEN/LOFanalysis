9c16fad905631a53a80f6c5b0e101af40b35b6cc
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-05-29 15:43:10 +0300

    keep only positive oddsratio

--- Scripts/analysis_results.py
+++ Scripts/analysis_results.py
@@ -72,4 +72,6 @@ def keep_single_pheno(iPath,lofString = 'hc_lof'):
 
     oFile =  iPath + lofString + '_gene_summary_ordered.txt'
     genes = np.loadtxt(oFile,dtype = str,usecols = [0])
-    return genes
+    from colection import Counter
+    c = Counter(genes)
+    return [gene for gene in c if c[gene] ==1]
