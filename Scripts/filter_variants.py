dcfe4c70918541f081484a9b93beafbd9e6b0acc
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-06-07 14:41:32 +0300

    changse

--- Scripts/filter_variants.py
+++ Scripts/filter_variants.py
@@ -27,9 +27,12 @@ def matrix_to_bool(iPath,lofString,infoScore=0.9):
     
     mFile = iPath + lofString + "_gene_to_sample.tsv"
     genes =  pd.read_csv(mFile,dtype = str,header = None,sep = '\t').values.flatten()
+    print('genes loaded.')
     with open(oFile,'wt') as o:
         for i,gene in enumerate(genes[:10]):
-            line = pd.read_csv(mFile,nrows = 1,skiprows = i,header = None).values.flatten()[1:]
+            sys.stdout.write('processing gene %s \r'%(gene)),
+            sys.stdout.flush()
+            line = pd.read_csv(mFile,nrows = 1,skiprows = i,header = None,dtype = float).values.flatten()[1:]
             line = np.where(line < infoScore,0,1).astype(str)
             line = np.concatenate(np.array([gene]),line)
             o.write("\t".join(gArray) + '\n')
