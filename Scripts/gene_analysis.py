79ee898b7a2ec0ea1af9abb0a35e72cfd718d045
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-06-11 11:02:43 +0300

    changes

--- Scripts/gene_analysis.py
+++ Scripts/gene_analysis.py
@@ -16,7 +16,7 @@ cpus = multiprocessing.cpu_count()
 # REQUIRED FILES
 phenoList = np.loadtxt(dataPath + 'pheno-list.txt',usecols = [0],dtype = str,skiprows = 1)
 phenoFile = dataPath + 'FINNGEN_PHENOTYPES_DF1_V4_2018_03_27.txt.gz'
-phenoFile = '/home/pete/Data/SPA_Data/R1_COV_PHENO.txt'
+phenoFile = '/home/pete/Data/SPA_data/R1_COV_PHENO.txt'
 #####################
 #--GENE  MULTIPROC--#
 #####################
