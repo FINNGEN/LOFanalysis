70971877a2bb3478e9614757eebe918d2226e616
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-06-12 11:39:03 +0300

    plotting

--- Scripts/analysis_results.py
+++ Scripts/analysis_results.py
@@ -17,8 +17,9 @@ miscPath = rootPath+ '/misc/'
 
 def qq_data(resPath ,lofString = 'hc_lof'):
 
-    resPath += '/' + lofString +'/'
-    files = get_filepaths(resPath +'/results/' )
+    resPath += '/' + lofString +'/results/'
+    print('fetching data from ' + resPath)
+    files = get_filepaths(resPath )
     res = []
     for f in files:
         print(f)
@@ -31,7 +32,7 @@ def qq_data(resPath ,lofString = 'hc_lof'):
                 except:
                     pass
     res = np.array(res)
-    np.savetxt(resPath +'_qq_data.txt',res,fmt = '%f')
+#    np.savetxt(resPath +'_qq_data.txt',res,fmt = '%f')
                 
     
     
