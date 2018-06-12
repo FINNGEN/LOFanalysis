53a6ba1cf7afcb814294e7661474090adb4bdb11
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-06-12 12:28:25 +0300

    plotting

--- Scripts/analysis_results.py
+++ Scripts/analysis_results.py
@@ -34,8 +34,8 @@ def qq_data(resPath ,lofString = 'hc_lof'):
                 except:
                     pass
                 
-#    res = np.array(res,dtype = np.float128)
-#    np.savetxt(qqPath,res,fmt = '%f')
+    res = np.array(res,dtype = np.float128)
+    np.savetxt(qqPath,res,fmt = '%f')
     return res
        
 def qq_plot(qqPath,fPath = figPath,lofString = 'hc_lof'):
