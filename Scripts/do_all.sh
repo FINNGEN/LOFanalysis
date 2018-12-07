ef9c952f91b408631c01b2204c43b1a13ae32a6b
piotor87 <pietro.dellabriottaparolo@gmail.com>
2018-12-07 11:29:23 +0200

    changes

--- Scripts/do_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-
-python gene_analysis.py --lof hc_lof --oPath ~/results/ fisher --infoScore 0.9 
-python gene_analysis.py --lof most_severe --oPath ~/results/ fisher --infoScore 0.9 
